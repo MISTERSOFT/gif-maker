31288da6cb3ff0bbce619306a31e6bbea052ef9c
MISTERSOFT <sofiane.hamadi.pro@gmail.com>
2019-03-12 21:19:21 +0100

    chore: create core module

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BrowserAnimationsModule,
+  ]
+})
+export class CoreModule { }
